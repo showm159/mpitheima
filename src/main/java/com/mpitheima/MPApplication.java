a2f1c471992f740023391213bbfc6f5d219369f5
showm159 <132239948+showm159@users.noreply.github.com>
2023-05-01 14:08:56 +0800

    Update MPApplication.java

--- src/main/java/com/mpitheima/MPApplication.java
+++ src/main/java/com/mpitheima/MPApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MPApplication {
     public static void main(String[] args) {
         SpringApplication.run(MPApplication.class,args);
-        System.out.println(111);
+        System.out.println(999);
         System.out.println(666);
     }
 }
