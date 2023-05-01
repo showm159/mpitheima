d2309a51d8983957c80185026fd23cbb649fd235
showm159 <132239948+showm159@users.noreply.github.com>
2023-05-01 14:05:01 +0800

    333

--- src/main/java/com/mpitheima/MPApplication.java
+++ src/main/java/com/mpitheima/MPApplication.java
@@ -9,5 +9,6 @@ public class MPApplication {
         SpringApplication.run(MPApplication.class,args);
         System.out.println(111);
         System.out.println(222);
+        System.out.println(333);
     }
 }
