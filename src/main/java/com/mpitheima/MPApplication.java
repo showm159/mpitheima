c6e82299ddc1df7c3e70ae90231ebb0e99a92f7e
showm159 <132239948+showm159@users.noreply.github.com>
2023-05-01 14:02:20 +0800

    111

--- src/main/java/com/mpitheima/MPApplication.java
+++ src/main/java/com/mpitheima/MPApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MPApplication {
     public static void main(String[] args) {
         SpringApplication.run(MPApplication.class,args);
+        System.out.println(111);
     }
 }
